e30e71ca962bee8d0d8cb5d0aa9887e28c04902c
Hoorad <hourrad.f@gmail.com>
2023-12-22 13:42:02 +1300

    Added ast functions

--- src/ast.c
+++ src/ast.c
@@ -1,1 +1,17 @@
 #include <ast.h>
+#include <stdlib.h>
+
+astree *newAst(void) {
+  astree *node = malloc(sizeof(astree));
+  node->type = 0;
+  node->left = NULL;
+  node->right = NULL;
+  return node;
+}
+
+void setData(astree *node, char *data) { node->data = data; }
+void setType(astree *node, enum astype type) { node->type = type; }
+void setChilds(astree *node, astree *l, astree *r) {
+  node->left = l;
+  node->right = r;
+}
--- src/include/ast.h
+++ src/include/ast.h
@@ -1,1 +1,21 @@
-typedef struct astree {}
+enum astype {
+  AST_PIPE,
+  AST_ARG,
+  AST_CMD,
+  AST_REDIN,
+  AST_REDOUT,
+  AST_BCKGRND,
+  AST_SEQ,
+};
+
+typedef struct astree {
+  enum astype type;
+  char *data;
+  struct astree *left;
+  struct astree *right;
+} astree;
+
+astree *newAst(void);
+void setData(astree *a, char *data);
+void setChilds(astree *a, astree *l, astree *r);
+void setNodeType(astree *a, enum astype nodetype);
