5f40a88960ff891074a977013d9fcd6e4350ffcc
Hoorad <hourrad.f@gmail.com>
2024-01-07 11:29:53 +1300

    Added parser functions

--- src/parser.c
+++ src/parser.c
@@ -7,9 +7,20 @@
 // Full Commnad ::= Pipe ';' Full Command | Pipe ';' | Pipe '&' | Pipe '&' Full
 // Command | Pipe
 // Pipe ::= Command | Command '|' Command
-// Command ::= Simple Command '>' Filename | Simple Command '<' Filename
+// Command ::= Simple Command '>' Filename | Simple Command '<' Filename |
+// Simple Command
 // Simple Command ::= Executable | Executable Arguments
 
-token *curtok = NULL;
+astree *FULL_COMMAND(void);
+astree *FULL_COMMAND1(void);
+astree *FULL_COMMAND2(void);
+astree *FULL_COMMAND3(void);
+astree *PIPE1(void);
+astree *PIPE2(void);
+astree *COMMAND1(void);
+astree *COMMAND2(void);
+astree *COMMAND3(void);
+astree *SIMPLE_COMMAND1(void);
+astree *SIMPLE_COMMAND2(void);
 
-void parse(lexer *l) { (void)l; }
+token *curtok = NULL;
